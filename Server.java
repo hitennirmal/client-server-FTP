0a1685529ebd9f240d8be5b97285f8746a6d63eb
Hiten Mahesh Nirmal <hitennirmal94@gmail.com>
2019-01-29 18:48:55 -0500

    Implemented pwd, ls,cd

--- Server.java
+++ Server.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 public class Server
 {
   public static void main(String[] args) throws Exception
@@ -17,10 +19,11 @@ public class Server
                               // receiving from server ( receiveRead  object)
       InputStream istream = sock.getInputStream();
       BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
-
-      String receiveMessage, sendMessage,rest,word;               
+       
+      String receiveMessage, sendMessage,rest,word,currentdir_temp;        
+      String currentdir=System.getProperty("user.dir");     
       while(true)
-      {    
+    {
         receiveMessage = receiveRead.readLine();
         while (receiveMessage!=null)
         {
@@ -28,7 +31,7 @@ public class Server
             if (index > -1) // Check if there is more than one word.
             { 
             word=receiveMessage.substring(0, index);// Extract first word.
-            rest= receiveMessage.substring(index);// rest word
+            rest= receiveMessage.substring(index+1);// rest word
             } else 
             {
             word=receiveMessage; // Text is the first word itself.
@@ -36,21 +39,41 @@ public class Server
             }
         if(receiveMessage != null && word.equals("pwd"))  
         {
-            System.out.println("Need Code for PWD");
-            pwrite.println("This will be reply from pwd");             
+            //System.out.println("Need Code for PWD");
+            pwrite.println(currentdir);             
             pwrite.flush();       
         }         
         else if(receiveMessage != null && word.equals("ls"))
         {
-
-            System.out.println("Need Code for ls");
-            pwrite.println("This will be reply from ls");             
-            pwrite.flush();
+            //System.out.println("Need Code for ls");
+            File dir = new File(currentdir);
+            String[] files = dir.list();
+            if (files.length == 0)
+             {
+                pwrite.println("The directory is empty");
+                
+            } else 
+            {
+                String str = String.join("    ", files);
+                pwrite.println(str);
+                }
+            pwrite.flush();           
         }
         else if (receiveMessage !=null && word.equals("cd"))
         {
-            System.out.println("Need Code for cd");
-            pwrite.println("This will be reply from cd");             
+            //System.out.println("Need Code for cd");
+            currentdir_temp=currentdir+ "/"+rest;
+            System.out.println(currentdir_temp);
+            if (Files.isDirectory(Paths.get(currentdir_temp))) 
+            {
+                pwrite.println("Directory Changed" );
+                currentdir=currentdir_temp;
+            }
+            else
+            {
+                pwrite.println("Directory does not exists");
+            }
+                         
             pwrite.flush();
         }
         
