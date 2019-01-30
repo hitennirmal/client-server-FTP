e5742ae572e0116c4e7cf76e11d476881a0541f9
Hiten Mahesh Nirmal <hitennirmal94@gmail.com>
2019-01-29 20:06:48 -0500

    Implemented
    Implemented the following with exception error
    get
    put
    mkdir 
    ls 
    cd 
    cd..
    delete

--- Server.java
+++ Server.java
@@ -2,6 +2,10 @@ import java.io.*;
 import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.DirectoryNotEmptyException;
+
 public class Server
 {
   public static void main(String[] args) throws Exception
@@ -20,7 +24,7 @@ public class Server
       InputStream istream = sock.getInputStream();
       BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
        
-      String receiveMessage, sendMessage,rest,word,currentdir_temp;        
+      String receiveMessage, sendMessage,rest,word,currentdir_temp, mkdir_temp;        
       String currentdir=System.getProperty("user.dir");     
       while(true)
     {
@@ -63,11 +67,11 @@ public class Server
         {
             //System.out.println("Need Code for cd");
             currentdir_temp=currentdir+ "/"+rest;
-            System.out.println(currentdir_temp);
             if (Files.isDirectory(Paths.get(currentdir_temp))) 
             {
                 pwrite.println("Directory Changed" );
                 currentdir=currentdir_temp;
+                System.out.println(currentdir);
             }
             else
             {
@@ -79,21 +83,55 @@ public class Server
         
         else if (receiveMessage !=null && word.equals("cd.."))
         {
-            System.out.println("Need Code for cd..");
-            pwrite.println("This will be reply from cd..");             
+           // System.out.println("Need Code for cd..");
+           currentdir=currentdir.substring(0,currentdir.lastIndexOf('/'));
+            pwrite.println("Directory Changed");             
             pwrite.flush();
+            System.out.println(currentdir);
+
         }
         else if (receiveMessage !=null && word.equals("mkdir"))
         {
-            System.out.println("Need Code for mkdir");
-            pwrite.println("This will be reply from mkdir");             
+            //System.out.println("Need Code for mkdir");
+            mkdir_temp=currentdir+"/"+rest;
+            File file = new File(mkdir_temp);
+            if (!file.exists()) 
+            {
+            if (file.mkdir()) 
+            {
+                pwrite.println("Directory is created!");
+                System.out.println("Created at "+ mkdir_temp);
+            } else 
+            {
+                pwrite.println("Failed to create directory!");
+            }
+             }
+            mkdir_temp=null;
             pwrite.flush();
         }   
         else if (receiveMessage !=null && word.equals("delete"))
-        {
-            System.out.println("Need Code for delete");
-            pwrite.println("This will be reply from delete");             
+        {   mkdir_temp=currentdir+"/"+rest;
+            //System.out.println("Need Code for delete");
+            try
+            { 
+                Files.deleteIfExists(Paths.get(mkdir_temp)); 
+            } 
+            catch(NoSuchFileException e) 
+            { 
+                pwrite.println("No such file/directory exists"); 
+            } 
+            catch(DirectoryNotEmptyException e) 
+            { 
+                pwrite.println("Directory is not empty."); 
+            } 
+            catch(IOException e) 
+            { 
+                pwrite.println("Invalid permissions."); 
+            } 
+              
+            pwrite.println("Deletion successful.");         
             pwrite.flush();
+            System.out.println("Deleted at"+ mkdir_temp);
         }   
         else if (receiveMessage !=null && word.equals("get"))
         {
